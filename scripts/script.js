74d665c5d601e44c858ab5a2c52d3879d523f7e1
ArishaMak <makovchikarina@icloud.com>
2025-07-14 17:24:04 +0300

    2-е ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,19 @@
 (function initTheme() {
-  const theme = localStorage.getItem('theme');
-  if (theme) {
-    setTheme(theme);
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    setTheme(savedTheme);
+  } else {
+    setTheme('auto');
   }
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
-  const currentTheme = [...document.documentElement.classList]
-    .find((cn) => cn.startsWith('theme-'))
-    ?.replace('theme-', '');
   const themeButtons = [
     ...document.querySelectorAll('.header__theme-menu-button'),
   ];
-  setActiveButton(themeButtons, currentTheme);
+
+  const currentActiveTheme = localStorage.getItem('theme') || 'auto';
+  setActiveButton(themeButtons, currentActiveTheme);
 
   themeButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -23,12 +24,31 @@ document.addEventListener('DOMContentLoaded', () => {
       setActiveButton(themeButtons, chosenTheme);
     });
   });
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+    if (localStorage.getItem('theme') === 'auto') {
+      applyAutoTheme();
+    }
+  });
 });
 
 function setTheme(theme) {
   document.documentElement.className = '';
-  document.documentElement.classList.add(`theme-${theme}`);
+
+  if (theme === 'auto') {
+    applyAutoTheme();
+  } else {
+    document.documentElement.classList.add(`theme-${theme}`);
+  }
   localStorage.setItem('theme', theme);
+}
+
+function applyAutoTheme() {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.classList.add('theme-dark');
+  } else {
+    document.documentElement.classList.add('theme-light');
+  }
 }
 
 function setActiveButton(buttonsArray, theme) {
@@ -36,17 +56,22 @@ function setActiveButton(buttonsArray, theme) {
     button.classList.remove('header__theme-menu-button_active');
     button.removeAttribute('disabled');
   });
+
   const target = buttonsArray.find((button) =>
     button.classList.contains(`header__theme-menu-button_type_${theme}`)
   );
+
   if (target) {
     target.classList.add('header__theme-menu-button_active');
     target.setAttribute('disabled', true);
-  } else {
+  }
+  else {
     const autoButton = document.querySelector(
       '.header__theme-menu-button_type_auto'
     );
-    autoButton.classList.add('header__theme-menu-button_active');
-    autoButton.setAttribute('disabled', true);
+    if (autoButton) {
+      autoButton.classList.add('header__theme-menu-button_active');
+      autoButton.setAttribute('disabled', true);
+    }
   }
 }
